6c86204d226a7a7ba1ed670989ef89dd10523db1
Nat Quayle Nelson <natquaylenelson@gmail.com>
2024-02-24 20:56:22 -0700

    higher-order function to solve a day & assert correctness

---
+++ main.ml
@@ -0,0 +1,19 @@
+let read_whole_file filename =
+    (* open_in_bin works correctly on Unix and Windows *)
+    let ch = open_in_bin filename in
+    let s = really_input_string ch (in_channel_length ch) in
+    close_in ch;
+    s
+
+let solve year day solver exampleResult =
+  let prefix = string_of_int year ^ "/" ^ string_of_int day in
+  let suffix = ".txt" in
+  let exampleFile = prefix ^ "example" ^ suffix in
+  let inputFile = prefix ^ suffix in
+  let eResult = solver (read_whole_file exampleFile) in
+  let realResult = solver (read_whole_file inputFile) in
+    assert (eResult = exampleResult);
+    Printf.printf "%d" realResult;;
+
+let day1 input = 0 in
+solve 2024 1 day1 142;
